fddd3eb5a8852183f6edac150b8645b49ce7f387
Jose Alan da Costa <allann.costa.ac@gmail.com>
2025-07-21 12:56:10 -0300

    Atualizando URL API

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.API_BASE_URL || 'https://organiza-back.vercel.app/',
+    baseURL: process.env.API_BASE_URL || 'https://organiza-back.onrender.com',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
